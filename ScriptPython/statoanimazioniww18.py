9e116b5f599e9a323534b355249b93cd09bf0b54
MrNico98 <136261932+MrNico98@users.noreply.github.com>
2025-01-27 16:31:51 +0100

    Update statoanimazioniww18.py

--- ScriptPython/statoanimazioniww18.py
+++ ScriptPython/statoanimazioniww18.py
@@ -11,9 +11,9 @@ topic_id = '26621'
 github_token = os.getenv('GITHUB_TOKEN')
 
 # URL dei file su GitHub
-mods_url = 'https://raw.githubusercontent.com/PianetaSimSito/PianetaSim/refs/heads/main/Json/animazioniww18.json'
-state_url = 'https://raw.githubusercontent.com/PianetaSimSito/PianetaSim/refs/heads/main/Json/telegramstato/last_stateanimazioni.json'
-repo_api_url = 'https://api.github.com/repos/PianetaSimSito/PianetaSim/contents/Json/telegramstato/last_stateanimazioni.json'
+mods_url = 'https://raw.githubusercontent.com/PianetaSimTS/PianetaSim/refs/heads/main/Json/animazioniww18.json'
+state_url = 'https://raw.githubusercontent.com/PianetaSimTS/PianetaSim/refs/heads/main/Json/telegramstato/last_stateanimazioni.json'
+repo_api_url = 'https://raw.githubusercontent.com/PianetaSimTS/PianetaSim/refs/heads/main/Json/telegramstato/last_stateanimazioni.json'
 
 # Funzione per scaricare un file JSON da un URL
 def fetch_json(url):
