916189e940af2480f79353eda0ad245b69f6fa11
MrNico98 <136261932+MrNico98@users.noreply.github.com>
2025-02-12 08:55:18 +0100

    Update statomod.py

--- ScriptPython/statomod.py
+++ ScriptPython/statomod.py
@@ -83,6 +83,21 @@ def compare_status_only(old_state, new_state):
     normalized_old = [normalize_mod(mod) for mod in old_state if normalize_mod(mod) is not None]
     normalized_new = [normalize_mod(mod) for mod in new_state if normalize_mod(mod) is not None]
 
+    # Identifica le nuove mod non presenti nello stato precedente
+    old_mod_names = {mod['ModName'] for mod in normalized_old}
+    for new_mod in normalized_new:
+        if new_mod['ModName'] not in old_mod_names:
+            icon = "🟣"  # Icona per mod nuove
+            new_mod['Status'] = "NUOVA"
+            
+            new_mod_message = (
+                f"MOD\n\n"
+                f"*{new_mod['ModName']}* ➜ Di *{new_mod['Author']}*\n\n"
+                f"Stato {icon} _{new_mod['Status']}_\n"
+                f"Link [SITO](https://pianetasimts.github.io/PianetaSim/index.html)"
+            )
+            messages.append(new_mod_message)
+
     # Mappa per lo stato con le relative icone
     status_icons = {
         "AGGIORNATA": "🟢",  # Pallino verde
